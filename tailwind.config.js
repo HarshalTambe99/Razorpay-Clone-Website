5b054522b63dc700d531d240b0ea8d2a95ee036a
HarshalTambe99 <114415010+HarshalTambe99@users.noreply.github.com>
2023-11-18 11:39:19 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        deepblue: "#02042a",
+      },
+    },
+  },
+  plugins: [],
+}
+
